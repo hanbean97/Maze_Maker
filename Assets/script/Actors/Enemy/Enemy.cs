cf54033abbba239b63ebf21e012b631958e0e75d
김인수 <>
2026-09-05 05:57:20 +0900

    gh

--- Assets/script/Actors/Enemy/Enemy.cs
+++ Assets/script/Actors/Enemy/Enemy.cs
@@ -11,7 +11,7 @@ public class Enemy : move
         stop,
         pathmove,
         attackReady, 
-        attack,
+        Ready,
         none,
     }
     [SerializeField] Movestate nowmove;
@@ -50,52 +50,39 @@ public class Enemy : move
     }*/
     protected virtual void Update()
     {
-        SearchEnemy();
-        FindingEnemy();
-        NowStateMode();
-
-        if (endTileOn == false)//마지막 라인에 도착하기 전까지
-        {
-            if ((transform.position.x > AsrarAlgo.instance.TargetPos.x - 1 && transform.position.x < AsrarAlgo.instance.TargetPos.x + 1) &&
-                               (-transform.position.y > AsrarAlgo.instance.TargetPos.y - 1 && -transform.position.y < AsrarAlgo.instance.TargetPos.y + 1))
-            {//마지막라인 도착
-                endTileOn = true;
+        if (startTileOn == false)
+        {// 자신이 스타트라인일 때 스타트라인까지 이동 
+            Moving();
+            if (transform.position.y < 0 || (transform.position.x > 9 || transform.position.x < 6))
+            {
+                startTileOn = true;
             }
-            if (startTileOn == true && targetEnemy == null)
-            {//스타트 라인이 아니고 적도 없을 때 
-
-                Moving(AsrarAlgo.instance.TargetPos);
-            }
-            else if (startTileOn == true && targetEnemy != null)
-            {// 적을 발견했고 그 자신이 스타트 라인이 아닐때 
-
-                targetPos = new Vector2Int(Mathf.RoundToInt(targetEnemy.position.x),Mathf.RoundToInt(-targetEnemy.position.y));
-                Moving(targetPos);
-            }
-            else if (startTileOn == false)
-            {// 자신이 스타트라인일 때 스타트라인까지 이동 
-                Moving();
-                if (transform.position.y < 0 || (transform.position.x > 9 || transform.position.x< 6 ))
-                {
-                    startTileOn = true;
-                }
-            }
-           
         }
+        if (endTileOn ==false&&(transform.position.x > AsrarAlgo.instance.TargetPos.x - 1 && transform.position.x < AsrarAlgo.instance.TargetPos.x + 1) &&
+                               (-transform.position.y > AsrarAlgo.instance.TargetPos.y - 1 && -transform.position.y < AsrarAlgo.instance.TargetPos.y + 1))
+        {//마지막라인 도착
+            endTileOn = true;
+        }
+
+       
         else if (endTileOn == true)
         {
             EndAction();
         }
+
+        if (startTileOn == false || endTileOn == true) return;//시작 부분과 끝 부분에서는 액션 금+
+        SearchEnemy();
+        FindAttackEnemy();
+        NowStateMode();
+
     }
 
-    
+
     void SearchEnemy()
     {
-        if (startTileOn == false) return;
 
-            
         count = GameManager.instance.NowMonstertrs.Count;
-        for (int i = 0; i < count; i++)
+        for (int i = 0; i < count; i++)//소환되어있는 몹 전체에 ray를 쏘고 거기에 닿으면 공격
         {
         dir = GameManager.instance.NowMonstertrs[i].position - transform.position;
         RaycastHit2D Searchrays = Physics2D.Raycast(transform.position, dir.normalized, Searchrange, LayerMask.GetMask("Wall", "Monster"));
@@ -108,27 +95,43 @@ public class Enemy : move
                 {//기존 타겟이 있으면 더 가까운 타겟을 넣는다
 
                    targetEnemy = Searchrays.transform;
+                   nowmove = Movestate.attackReady;
                 }
             }
             else
             {
-                    targetEnemy = Searchrays.transform;
+                targetEnemy = Searchrays.transform;
+                nowmove = Movestate.attackReady;
             }
            
         }
 
-            Transform meetTarget = GameManager.instance.MeetingTarget();
-            if (targetEnemy == null && meetTarget != null)
+            
+        }
+
+        Transform meetTarget = GameManager.instance.MeetingTarget();
+        if (targetEnemy == null && meetTarget != null)//다른 아군이 싸우고 있으면 그걸 타깃으로 벗 대기 상태도 여기
+        {
+            Vector3 meetdir = meetTarget.position - transform.position;
+            RaycastHit2D meetrays = Physics2D.Raycast(transform.position, meetdir.normalized, LayerMask.GetMask("Wall", "Monster"));
+            if(meetrays && meetrays.transform.CompareTag("Monster"))
             {
                 targetEnemy = meetTarget;
                 nowmove = Movestate.attackReady;
             }
+            else
+            {
+                nowmove = Movestate.stop;//대기 전투중이지만 다가갈 방법이 없을 때 
+            }
         }
-    }
-    void FindingEnemy()//적공격
-    {//이 부분에서 타깃 지정 슬롯 적용 타깃 주변에서
-        if (startTileOn == false) return;//시작지점에서는 액션금지
+        else if(targetEnemy == null && meetTarget == null)
+        {
+            nowmove = Movestate.pathmove;
+        }
 
+    }
+    void FindAttackEnemy()//적공격
+    {//이 부분에서 타깃 지정 슬롯 적용 타깃 주변에서
 
         if (targetEnemy != null)
         {
@@ -137,9 +140,8 @@ public class Enemy : move
             switch (enemytype)
             {
                 case EnemyTypelist.meleeE://근접 사거리 안에 다가가기만 해도 공격 자리를 잡기위해 움직인다 
-                    meleeEAttackReady();
 
-                    if (Vector3.Distance(transform.position, targetEnemy.position) < attackrange)
+                    if (Vector3.Distance(transform.position, targetEnemy.position) < attackrange+0.2f)//혹시 모를 오차범위 
                     {
                         attackGo();
                         fightend = true;
@@ -162,6 +164,8 @@ public class Enemy : move
 
         if(targetEnemy == null && fightend == true)//전투가 끝날 때
         {
+            attackStop();
+            nowmove = Movestate.stop;
             fightend = false;
             int count = GameManager.instance.Nowenemytrs.Count;
             for (int i = 0; i < count; i++)
@@ -191,13 +195,16 @@ public class Enemy : move
 
         int slot = Mathf.RoundToInt(angle / 45f) % 8; // 0~45: 0 , 45~90: 1, 90~180:2 ...
         int a = 0;
-        
+        bool fightpossible = false;
+        Vector2 attackpointer;
+        Vector3 attackslot = Vector3.zero;
+
         for (int i = 0; i < 3; i++)// 앞의 3방향만 탐새
         {
 
-            Vector2 attackpointer = targetEnemy.position;
+            attackpointer = targetEnemy.position;
 
-            a = a + (i * (i % 2 == 1 ? -1 : 1));//다음 탐색할 슬.
+            a = a + (i * (i % 2 == 1 ? -1 : 1));//다음 탐색할 슬롯 
             int nowslot = (a + slot + 8) % 8;
 
             if (nowslot < 2 || nowslot > 6)
@@ -217,38 +224,46 @@ public class Enemy : move
             {
                 attackpointer.y += 1 * attackrange;
             }
-            Vector3 attackslot = attackpointer - new Vector2(transform.position.x, transform.position.y);
+
+            if(attackpointer.x != targetEnemy.position.x && attackpointer.y != targetEnemy.position.y )
+            {
+                attackpointer.x *= 1.4f;
+                attackpointer.y *= 1.4f;
+            }
+
+            attackslot = attackpointer - new Vector2(transform.position.x, transform.position.y);
             float pointdis = Vector2.Distance(attackpointer, transform.position);
             RaycastHit2D ray = Physics2D.Raycast(transform.position, attackslot.normalized, pointdis, LayerMask.GetMask("Wall", "Monster"));
 
-            if (!ray.transform.CompareTag("wall"))
+            if (!ray || !ray.transform.CompareTag("wall"))
             {
-
-                anim.SetBool("Run", true);
-
-                transform.position += attackslot.normalized * speed * Time.deltaTime;
-                if (attackslot.normalized.x > 0)
-                {
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                }
-                else
-                {
-                    transform.rotation = Quaternion.Euler(0, 180, 0);
-                }
+                fightpossible = true;
+                break;
 
             }
            
         }
-        if (Vector2.Distance(transform.position, targetEnemy.position) <= attackrange + 0.2f)
+        if(fightpossible == true)// 갈 수 있는 곳이 있다면 이동
         {
-            attackGo();
+            anim.SetBool("Run", true);
+
+            transform.position += attackslot.normalized * speed * Time.deltaTime;
+            if (attackslot.normalized.x > 0)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+            }
+
         }
+
     }
 
 
    void NowStateMode()
     {
-        if (startTileOn == false) return;
 
         switch (nowmove)
         {
@@ -259,56 +274,23 @@ public class Enemy : move
             case Movestate.pathmove:// 미궁 길찾기 정보에 따라 이동
                 ismoveway = true;
                 Moving(AsrarAlgo.instance.TargetPos);
-                attackStop();
+                
 
                 break;
 
             case Movestate.attackReady://레이를 쏴서 몬스터의 슬롯을 정하고 자리이동 이동 불가시 정지 ,해야할거 게임 매니저에서 전투시 정지 관리
                 ismoveway = false;
-                switch (enemytype)
+
+                if(enemytype == EnemyTypelist.meleeE)
                 {
-                    case EnemyTypelist.meleeE://근접 사거리 안에 다가가기만 해도 공격 자리를 잡기위해 움직인다 
-                        meleeEAttackReady();
-
-                    break;
-                    case EnemyTypelist.RangedE://원거리 목표사이에 벽이 없으면 공격 자리를 잡기위해 움직이지 않는다
-
-                        Vector3 targetdir = targetEnemy.position - transform.position;
-                        RaycastHit2D Searchrays = Physics2D.Raycast(transform.position, targetdir.normalized, attackrange, LayerMask.GetMask("Wall", "Monster"));
-                        if (Searchrays && Searchrays.transform.CompareTag("Monster"))
-                        {
-                            nowmove = Movestate.attack;
-                        }
-
-                    break;
+                    meleeEAttackReady();//근접으로 이동 
                 }
-
-                if(targetEnemy == null)
+                else
                 {
-                    nowmove = Movestate.pathmove;
+
                 }
-                
                 break;
-            case Movestate.attack://지금 순서가 엉망진창 ->타겟 변경 되면 ? 이걸 스테이트로 조절하는게 아니라 공격따로 이동 따로 조절 전체 해당 함수 삭제하고 수정요/
-                ismoveway = false;
-                attackGo();
-
-                if (targetEnemy == null)//타겟이 사라졌으면 이동 
-                {
-                    nowmove = Movestate.stop;
-
-                    int count = GameManager.instance.Nowenemytrs.Count;
-                    for (int i = 0; i < count; i++)
-                    {
-                        if (GameManager.instance.Nowenemytrs[i] == this)//이 유닛의 소환 순서 탐색후 그에 따라 출발 
-                        {
-                            Invoke(nameof(delaystartgo), i * 0.5f);
-                        }
-                    }
-                }
-
-                break;
-           
+          
         }
     }
 
