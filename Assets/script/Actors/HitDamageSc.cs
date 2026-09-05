f69e8ca7320b9a7e4800b4f31fa3605900f6ddd7
hanbean97 <coldbean97@gmail.com>
2026-09-05 22:12:57 +0900

    ewrds

--- Assets/script/Actors/HitDamageSc.cs
+++ Assets/script/Actors/HitDamageSc.cs
@@ -6,5 +6,14 @@ public class HitDamageSc : MonoBehaviour
 {
     [SerializeField]float Damage;
     public float GetDamage { get { return Damage; } }
-
+    bool hitthis =false;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("Monster") &&hitthis == false)
+        {
+            hitthis = true;
+            other.GetComponent<Monster>().GetDamage(Damage);
+            Destroy(this);
+        }
+    }
 }
