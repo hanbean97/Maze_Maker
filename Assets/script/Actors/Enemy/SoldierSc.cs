c9dfec56a944d5db23ace3aed637bb7d7896853d
김인수 <>
2026-09-07 06:29:44 +0900

    h

---
+++ Assets/script/Actors/Enemy/SoldierSc.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierSc : Enemy
+{
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+    protected override void attackGo()
+    {
+    }
+    protected override void attackStop()
+    {
+    }
+
+    public void SoldierAttack()
+    {
+        if(targetEnemy != null)
+        {
+            targetEnemy.GetComponent<Monster>().GetDamage(attackDamage);
+        }
+    }
+
+}
